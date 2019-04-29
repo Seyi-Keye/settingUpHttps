c371f32ae7295ff1bfc746c12c6bea4d21308d89
Seyi-Keye <oluwaseyi.aromokeye@andela.com>
2019-04-29 13:30:56 +0100

    set up https server

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const https = require('https');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const certOptions = {
+  key: fs.readFileSync(path.resolve('build/cert/server.key')),
+  cert: fs.readFileSync(path.resolve('build/cert/server.crt'))
+};
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+const server = https.createServer(certOptions, app).listen(3000, () => console.log('App listening on port 3000'));
